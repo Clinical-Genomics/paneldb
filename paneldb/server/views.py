bfec8978eb395e7220cdb112fabedf5284165d70
northwestwitch <rasi.chiara@gmail.com>
2018-06-26 16:32:24 +0200

    added upload file form for panels and baitsets

--- paneldb/server/views.py
+++ paneldb/server/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+import logging
+from flask import render_template, flash, request, redirect
+from werkzeug.utils import secure_filename
 from paneldb import app
+
+LOG = logging.getLogger(__name__)
 
 @app.route('/')
 def index():
@@ -10,6 +14,20 @@ def index():
 def gene_panels():
     return render_template("panels.html")
 
-@app.route('/baitsets')
+@app.route('/baitsets', methods=['GET', 'POST'])
 def baitsets():
-    return render_template("baitsets.html")
+    LOG.info("IN BAITSETS")
+    document=None
+    if request.method == 'POST': #add new baitset
+        if 'inputFile' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        baits_file = request.files['inputFile']
+        if baits_file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        else:
+            document=secure_filename(baits_file.filename)
+
+    return render_template("baitsets.html", document=document)

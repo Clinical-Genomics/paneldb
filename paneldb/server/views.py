d3e32f4e1bdfd661c61b65f6b8c9323e8924d9aa
Emma Sernstad <emma.sernstad@scilifelab.se>
2018-09-04 11:21:09 +0200

    Added first steps to save file

--- paneldb/server/views.py
+++ paneldb/server/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 from flask import render_template, flash, request, redirect
 from werkzeug.utils import secure_filename
 from paneldb import app
@@ -16,18 +17,52 @@ def gene_panels():
 
 @app.route('/baitsets', methods=['GET', 'POST'])
 def baitsets():
-    LOG.info("IN BAITSETS")
-    document=None
+    """Handles the submission of baitsets"""
+
+    document = None
+    data = {}
     if request.method == 'POST': #add new baitset
+        # check if the post request has the file part
         if 'inputFile' not in request.files:
             flash('No file part')
             return redirect(request.url)
 
         baits_file = request.files['inputFile']
+
+        # if user does not select a file, browser submit an empty part without filename
         if baits_file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         else:
-            document=secure_filename(baits_file.filename)
+            # Parse the file to check that it's OK
+            # Extract fields to be passes to the DB
+            # Pass stuff to DB and get all updated baitsets in it.
 
-    return render_template("baitsets.html", document=document)
+            document = secure_filename(baits_file.filename)
+
+            #check that the file extension is correct
+            extension_result = allowed_file(document)
+
+            if extension_result:
+                    data = {
+                        'document': document,
+                        'upload_folder': app.config['UPLOAD_FOLDER'],
+                        'allowed_extensions' : app.config['ALLOWED_EXTENSIONS'],
+                        'in_extensions' : extension_result
+                    }
+
+            else:
+                flash('File '+str(document)+' has a wrong extension', 'danger')
+                return redirect(request.url)
+
+    return render_template("baitsets.html", **data)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+#def savefile:
+#    """Saves a file in a temporary folder"""
+
+

bd057af7b1c90ecc1f391e120609aae2a3cd6ecf
northwestwitch <rasi.chiara@gmail.com>
2018-09-25 16:23:22 +0200

    introduced flash messages and created create baitset method

--- paneldb/server/controllers.py
+++ paneldb/server/controllers.py
@@ -1,1 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+import pymongo
+LOG = logging.getLogger(__name__)
+from paneldb.parser.baitparser import read_file
 
+def save_baitset(adapter, name, version, temp_path, build='GRCh37'):
+    """Calls adapter functions to insert a baitset and its baits into database
+
+    Args:
+        name(str): name of the baitset to be saved
+        version(str): baitset version
+        temp_path(str): path to temporary baitset_file
+
+    Returns:
+        result(dict): baitset object inserted into database
+
+    """
+    LOG.info("saving baitset {}.{} to database".format(name, version))
+    #read from baitset file and collect baits
+    baits_list = read_file(temp_path)
+    if baits_list:
+        return adapter.add_baitset(name=name, version=version, baits=baits_list, build=build)
+    else:
+        return None
+
+
+def get_baitsets(adapter):
+    """Gets all available baitsets from database"""
+
+    LOG.info('Getting available baitsets')
+    baitsets = list(adapter.baitsets()) # convert pymongo.Cursor to object list
+
+    data = {
+        'baitsets' : baitsets,
+    }
+
+    return data
--- paneldb/server/views.py
+++ paneldb/server/views.py
@@ -2,9 +2,11 @@
 import logging
 import os
 from flask import render_template, flash, request, redirect
-from werkzeug.utils import secure_filename
 from paneldb import app
 from paneldb.parser.baitparser import read_file
+from . import controllers
+
+adapter = app.adapter
 
 LOG = logging.getLogger(__name__)
 
@@ -18,7 +20,7 @@ def gene_panels():
 
 @app.route('/baitsets', methods=['GET', 'POST'])
 def baitsets():
-    """Handles the submission of baitsets"""
+    """Handles the baitsets page"""
 
     document = None
     data = {}
@@ -28,27 +30,29 @@ def baitsets():
             return redirect(request.url)
 
         baits_file = request.files['inputFile']
+        document = baits_file.filename
 
         # if user does not select a file, browser submit an empty part without filename
-        if baits_file.filename == '':
-            flash('No selected file')
+        if document == '':
             return redirect(request.url)
-
         else:
-            savefile(baits_file ,document)
+            path_to_temp_file = os.path.join(app.config['UPLOAD_FOLDER'], document)
 
-            #get content from file:
-            lines = read_file(os.path.join(app.config['UPLOAD_FOLDER'], document))
+            #save baisets file into temp directory
+            save_file(baits_file, path_to_temp_file)
 
-            data = {
-                'document': document,
-                'upload_folder': app.config['UPLOAD_FOLDER'],
-                'baitlist' : lines
-            }
+            #call controllers to save baitset to database
+            new_baitset_id = controllers.save_baitset(adapter, name=request.form.get('baitset_name'), version='1.0', temp_path=path_to_temp_file, build=request.form.get('chr_build'))
+            flash("New baitset id: "+str(new_baitset_id))
 
+            #remove the temp baitset file once everything is saved
+            os.remove(path_to_temp_file)
+
+    data = controllers.get_baitsets(adapter)
     return render_template("baitsets.html", **data)
 
 
-def savefile(baits_file, filename):
-    """Saves a file in a temporary folder"""
-    baits_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+def save_file(baits_file, full_file_path):
+    """Saves a file to temp directory"""
+    baits_file.save(full_file_path)

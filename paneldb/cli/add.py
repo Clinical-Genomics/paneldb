a420778a5fa5d0bbf4abbe03c461793a02920bfa
northwestwitch <rasi.chiara@gmail.com>
2018-11-27 15:13:41 +0100

    fixed code and created progress bar

--- paneldb/cli/add.py
+++ paneldb/cli/add.py
@@ -2,6 +2,7 @@
 import logging
 import click
 import pymongo
+import enlighten
 from pymongo import MongoClient
 from paneldb.parser.baitparser import baits
 from paneldb.adapter.mongo import PanelAdapter
@@ -20,35 +21,39 @@ LOG = logging.getLogger(__name__)
 def add_baitset( file, db_uri, email, panel_name, version, genomic_build):
 
     LOG.info("saving baitset {}.{} to database".format(panel_name, version))
-    try:
 
-        client = MongoClient(db_uri)
-        db_name = db_uri.split('/')[-1]
-        db = client[db_name]
-        adapter = PanelAdapter(client=client, db_name=db_name)
+    client = MongoClient(db_uri)
+    db_name = db_uri.split('/')[-1]
+    db = client[db_name]
+    adapter = PanelAdapter(client=client, db_name=db_name)
 
-        created_baitset_id = adapter.add_baitset(name=panel_name, version=version, build=genomic_build)
+    created_baitset_id = adapter.add_baitset(name=panel_name, version=version, build=genomic_build)
 
-        # obtain all baits from this baitset
+    inserted_baits = 0
+
+    # if baitset creation was successful, insert baits
+    if created_baitset_id:
+
+        # obtain all baits from this baitset, formatted as objects
         baits_list = baits(path_to_file=file, created_baitset_id=created_baitset_id)
 
+        # insert one bait at the time into db
+        pbar = enlighten.Counter(total=len(baits_list), desc='', unit='ticks')
         for bait in baits_list:
             try:
                 # add the bait
                 inserted_bait_id = adapter.add_bait(bait)
-                LOG.info(type(inserted_bait_id))
+                inserted_baits += 1
 
             except pymongo.errors.DuplicateKeyError:
+                # if bait exists do nothing
                 LOG.info('pymongo DuplicateKeyError')
 
-            except Exception as err: #other type of error
-                LOG.info(type(err))
-
             finally:
-                if created_baitset_id:
-                    updated_baitset = db.baitset.find_one_and_update( {'_id': created_baitset_id}, {'$push': {'baits':inserted_bait_id}}, upsert= True )
+                # update baitset with list of baits contained in it
+                updated_baitset = db.baitset.find_one_and_update( {'_id': created_baitset_id}, {'$push': {'baits':bait['_id']}}, upsert= True )
+            pbar.update()
 
-        #LOG.info(baits_list)
-
-    except Exception as err:
-        LOG.error("An error occurred while saving from baitset file: {}".format(err))
+        LOG.info('created baitset with ID {0}. Inserted {1} out of {2} new available baits into db'.format(created_baitset_id, inserted_baits, len(baits_list)))
+    else:
+        LOG.error("Something went wrong and the baitset coudn't be saved to db.")
